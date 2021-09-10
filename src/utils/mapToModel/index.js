610103495b445bb12c9578e486a3bff5813bf27d
viraannisaa99 <viraannisaa99@gmail.com>
2021-09-10 11:08:24 +0700

    using rest parameter

--- src/utils/mapToModel/index.js
+++ src/utils/mapToModel/index.js
@@ -1,20 +1,10 @@
 /* eslint-disable camelcase */
 const mapDBToSongModel = ({ 
-  id, 
-  title, 
-  year, 
-  performer, 
-  genre, 
-  duration, 
   inserted_at, 
-  updated_at 
+  updated_at, 
+  ...args 
 }) => ({
-  id,
-  title,
-  year,
-  performer,
-  genre,
-  duration,
+  ...args,
   insertedAt: inserted_at,
   updatedAt: updated_at,
 });
